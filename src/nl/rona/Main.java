ac9045547cbf780c3bce085bc86d9377c3327903
DilaDamla <38209259+DilaDamla@users.noreply.github.com>
2021-05-12 19:43:36 +0200

    tweede taak is toegevoegd en daarmee is de user story voltooid

--- src/nl/rona/Main.java
+++ src/nl/rona/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Taak 1 is voltooid");
+        System.out.println("Taak 2 is voltooid en BTW daarmee de user story");
     }
 }
